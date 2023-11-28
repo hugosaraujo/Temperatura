0f73ef9fc80d4b239bd66d47360918e68621a157
hugo de araujo <hugo.saraujo@tutanota.com>
2023-11-28 20:55:20 -0300

    Conversão de tipo de Celsius para Fahrenheit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        double temperaturaEmCelsius = 27.5;
+        double temperaturaEmFahrenheit = ((temperaturaEmCelsius * 1.8) + 32);
+
+        String textoTemperatura = """
+                   Temperatura em Celsius: %.1f
+                   Temperatura em Fahrenheit: %.1f
+                """.formatted(temperaturaEmCelsius, temperaturaEmFahrenheit);
+
+        System.out.println(String.format(textoTemperatura));
 
     }
 }
